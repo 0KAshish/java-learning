2b70580aa8c5d566b8c1bbf2141bbffd60c7b6ad
Ashish Kumar <ashishmaurya.dev.in@gmail.com>
2025-11-26 08:54:09 +0000

    Started rock paper seissor game

---
+++ rock-paper-scissors/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        //This is Rock Paper and Scissors game in java
+        System.out.println("Welcome to the game");
+    }
+}
